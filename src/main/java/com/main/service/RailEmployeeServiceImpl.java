dae7340fdbefc22b890885d00b245d1882097bd5
CodeAruninverse <kumarun009@gmail.com>
2024-09-09 21:44:26 +0530

    Latest

---
+++ src/main/java/com/main/service/RailEmployeeServiceImpl.java
@@ -0,0 +1,33 @@
+package com.main.service;
+
+import java.util.List;
+
+import com.main.entity.RailEmployee;
+
+public class RailEmployeeServiceImpl implements RailEmployeeService {
+
+	@Override
+	public RailEmployee saveEmployee(RailEmployee re) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<RailEmployee> saveAllEmployee(List<RailEmployee> re) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<RailEmployee> getAllEmployee() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public RailEmployee getEmployeeById(String employeeId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
